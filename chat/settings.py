9e868dd3cb17c7e1bd9e63b91080dc5a2c359336
AlexandrIzevsk <sasha-sarov-333@rambler.ru>
2025-03-28 17:59:03 +0400

    chat: for help

--- chat/settings.py
+++ chat/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,9 +48,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.yandex',
 
-    'app',
+
     'accounts',
-    'channels',
+    # 'channels',
     'rest_framework',
 ]
 
